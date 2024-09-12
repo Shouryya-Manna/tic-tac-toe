a01b2dfee19186fd0b0002df0532bf00acce5f8d
Shouryya Manna <shouryyamanna@gmail.com>
2024-09-12 21:39:36 +0530

    wincondition implemented and made the winning line green

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,34 +1,78 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Box from "./Box";
 function TicTacToe() {
+  const [board, setBoard] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+  const [player, setPlayer] = useState("X");
+  const [winningLine, setWinningLine] = useState([]);
 
-  const [player,setPlayer] = useState("X");
+  useEffect(() => {
+    winCondition()
+  }, [board]);
 
-  function handleShow(index){
-    let updatedBoard = [...board];
-    updatedBoard[index]=player;
-    setBoard(updatedBoard);
-    if(player=="X"){
-      setPlayer("O");
-    }
-    else{
-      setPlayer("X");
+  function winCondition(){
+    const win = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    let i = 0;
+    for (i = 0; i < win.length; i++) {
+      if (
+        (board[win[i][0]] == "X" &&
+          board[win[i][1]] == "X" &&
+          board[win[i][2]] == "X") ||
+        (board[win[i][0]] == "O" &&
+          board[win[i][1]] == "O" &&
+          board[win[i][2]] == "O")
+      ) {
+        setWinningLine(win[i]);
+      }
     }
   }
 
 
-  const [board,setBoard] = useState([null, null, null, null, null, null, null, null, null]);
-    return (
-      <div className='flex flex-col justify-center items-center'>
-        <h1 className="text-5xl mt-[100px]">Tic Tac Toe</h1>
-        <div className="items-center mt-[100px] grid gap-3 grid-cols-3 grid-rows-3 ">
-          {board.map((value, index) => (
-            <div className="flex w-[100px] h-[100px] border-2 justify-center items-center " 
+  function handleShow(index) {
+    let updatedBoard = [...board];
+    updatedBoard[index] = player;
+    setBoard(updatedBoard);
+    if (player == "X") {
+      setPlayer("O");
+    } else {
+      setPlayer("X");
+    }
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-5xl mt-[100px]">Tic Tac Toe</h1>
+      <div className="items-center mt-[100px] grid gap-3 grid-cols-3 grid-rows-3 ">
+        {board.map((value, index) => (
+          <div
+            className={`flex w-[100px] h-[100px] border-2 justify-center items-center 
+              ${(winningLine.includes(index))?"bg-green-200":""} `}
             key={index}
-            onClick={()=>handleShow(index)}>{value}</div>
-          ))}
-        </div>
-      </div>                  
-    );
+            onClick={() => handleShow(index)}
+          >
+            {value}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 export default TicTacToe;
